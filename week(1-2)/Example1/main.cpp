d294a14e74a15e6ac2a41c80a562f62b50597fa9
Delta <shatalovlev@mail.ru>
2022-04-04 21:20:29 +0300

    Example1

---
+++ week(1-2)/Example1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//Вывести на экран Первую букву имени из символов "*" (звездочка)
+
+int main() {
+    std::cout << "     *" << std::endl
+        << "    * *" << std::endl
+        << "   *   *" << std::endl
+        << "  *     *" << std::endl
+        << " *       *" << std::endl;
+    return 0;
+}

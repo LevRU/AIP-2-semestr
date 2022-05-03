1819fc41ca16b93f8c05a0b1fbed8afe34b4f95f
Delta <shatalovlev@mail.ru>
2022-05-03 12:21:01 +0300

    Example3

---
+++ week(1-2)/Example3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+int main() {
+    float a;
+    float b;
+    std::cout << " Enter a&b  "<< std::endl;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout <<" a - b = "<< a-b<< std::endl;
+    std::cout <<"a / b = "<<a/b;
+    return 0;
+}
